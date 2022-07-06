3596912363518cf3f992403401a76fa7d6abdf93
Ian <80196814+Screll3@users.noreply.github.com>
2022-07-06 17:51:37 +0700

    19.5
    V1.0

---
+++ 19.5/19.5.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+class animal
+{
+public:
+    animal(){}
+    animal(std::string _s) : s(_s) {}
+    void voice()
+    {
+        std::cout << s << std::endl;
+    }
+private:
+    std::string s;
+};
+
+class dog : public animal
+{
+public:
+    dog() : animal("Woof") {}
+};
+
+class cat : public animal
+{
+public:
+    cat() : animal("Meow") {}
+};
+
+class duck : public animal
+{
+public:
+    duck() : animal("Quack") {}
+};
+
+int main()
+{
+    animal* arr = new animal[3];
+    arr[0] = dog();
+    arr[1] = cat();
+    arr[2] = duck();
+
+    for (int i = 0; i < 3; i++)
+    {
+        arr[i].voice();
+    }
+    return 0;
+}
